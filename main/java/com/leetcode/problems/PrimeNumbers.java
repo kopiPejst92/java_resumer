a8a5c385064993010b47970df8d52bf4b0e89c89
Aga <agkop01@gmail.com>
2026-04-20 18:12:19 +0200

    prime numbers

---
+++ main/java/com/leetcode/problems/PrimeNumbers.java
@@ -0,0 +1,25 @@
+package com.leetcode.problems;
+
+public class PrimeNumbers {
+
+    public static boolean isPrime(int num){
+        if(num<2) return false;
+        else if(num==2 || num==3){
+            return true;
+        }
+        else{
+            int sqrt = (int) Math.ceil(Math.sqrt(num));
+            int i=2;
+            while(i<=sqrt){
+                if(num%i==0) return false;
+                i+=1;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        int notPassed=15342889;
+        System.out.println(PrimeNumbers.isPrime(notPassed));
+    }
+}
